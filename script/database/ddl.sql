5c714606161e30db2bd6e632b778a7dbc691513e
alerario <alerario@gmail.com>
2021-10-04 07:35:49 -0300

    Create ddl.sql

---
+++ script/database/ddl.sql
@@ -0,0 +1,23 @@
+-- crie o BD oficina 
+-- Exemplo de criacao de tabelas
+    
+CREATE TABLE IF NOT EXISTS public.cidade
+(
+    id SERIAL,
+    nome character varying(80) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT cidade_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.cliente
+(
+    id SERIAL,
+    nome character varying(80) COLLATE pg_catalog."default",
+    cidade integer,
+    CONSTRAINT cliente_pkey PRIMARY KEY (id),
+    CONSTRAINT fg_cid FOREIGN KEY (cidade)
+        REFERENCES public.cidade (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+-- fim do exemplo ---
